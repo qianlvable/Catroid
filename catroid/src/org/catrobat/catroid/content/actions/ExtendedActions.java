3e124e22510b1dd82da7227a9e12ffdbb4b5cd70
Adrian Schnedlitz <a.schnedlitz@student.tugraz.at>
2014-05-26 14:39:26 +0200

    Changed Arduino Sensors to Functions

--- catroid/src/org/catrobat/catroid/content/actions/ExtendedActions.java
+++ catroid/src/org/catrobat/catroid/content/actions/ExtendedActions.java
@@ -361,22 +361,6 @@ public class ExtendedActions extends Actions {
 		return action;
 	}
 
-	public static ArduinoAnalogPinSensorAction arduinoAnalogPinSensorAction(Sprite sprite, Formula variableFormula,
-			UserVariable userVariable) {
-		ArduinoAnalogPinSensorAction action = action(ArduinoAnalogPinSensorAction.class);
-		action.setSprite(sprite);
-		action.setUserVariable(userVariable);
-		return action;
-	}
-
-	public static ArduinoDigitalPinSensorAction arduinoDigitalPinSensorAction(Sprite sprite, Formula variableFormula,
-			UserVariable userVariable) {
-		ArduinoDigitalPinSensorAction action = action(ArduinoDigitalPinSensorAction.class);
-		action.setSprite(sprite);
-		action.setUserVariable(userVariable);
-		return action;
-	}
-
 	public static TemporalAction droneTakeOff() {
 		return action(DroneTakeoffAction.class);
 	}
