218331a4b6a3d5f44bbafcf4f70a512130d45eb8
Martin Burtscher <martin.burtscher@student.tugraz.at>
2014-10-21 16:36:22 +0200

    added MindstormConnection test

--- catroidTest/src/org/catrobat/catroid/test/mindstorms/nxt/MindstormConnectionTest.java
+++ catroidTest/src/org/catrobat/catroid/test/mindstorms/nxt/MindstormConnectionTest.java
@@ -23,5 +23,101 @@
 
 package org.catrobat.catroid.test.mindstorms.nxt;
 
-public class MindstormConnectionTest {
+import android.test.AndroidTestCase;
+
+import org.catrobat.catroid.lego.mindstorm.MindstormConnectionImpl;
+import org.catrobat.catroid.lego.mindstorm.nxt.Command;
+import org.catrobat.catroid.lego.mindstorm.nxt.CommandByte;
+import org.catrobat.catroid.lego.mindstorm.nxt.CommandType;
+import org.catrobat.catroid.test.utils.Reflection;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+public class MindstormConnectionTest extends AndroidTestCase {
+
+	public static final int HEADER_SIZE = 2;
+
+	public void testSend() {
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+		MindstormConnectionImpl connection = new MindstormConnectionImpl(null);
+		Reflection.setPrivateField(connection, "nxtOutputStream", outStream);
+
+		Command command = new Command(CommandType.DIRECT_COMMAND, CommandByte.SET_OUTPUT_STATE, false);
+		command.append((byte)0x1);
+		command.append((byte)0x2);
+		command.append((byte)0x3);
+
+		connection.send(command);
+
+		byte[] sentBytes = outStream.toByteArray();
+
+		byte[] expectedMessage = command.getRawCommand();
+
+		assertEquals("Wrong message length. Before there should be a header with 2 bytes defining the message length",
+				expectedMessage.length + HEADER_SIZE, sentBytes.length);
+		assertEquals("Header should be the size of the message.", expectedMessage.length, sentBytes[0]);
+		assertEquals(0, sentBytes[1]);
+
+		for (int i = 0; i < expectedMessage.length; i++) {
+			assertEquals("Byte " + i + " is different", expectedMessage[i], sentBytes[i + HEADER_SIZE]);
+		}
+	}
+
+	public void testReceive() {
+		byte[] inputBuffer = new byte[] {4, 0, 3, 4, 5, 7};
+		ByteArrayInputStream inStream = new ByteArrayInputStream(inputBuffer);
+
+		MindstormConnectionImpl connection = new MindstormConnectionImpl(null);
+		Reflection.setPrivateField(connection, "nxtInputStream", inStream);
+
+		byte[] receivedBytes = connection.receive();
+
+		assertEquals("Wrong message length. Before there should be a header with 2 bytes defining the message length",
+				inputBuffer.length - HEADER_SIZE, receivedBytes.length);
+
+		for (int i = 0; i < receivedBytes.length; i++) {
+			assertEquals("Byte " + i + " is different", inputBuffer[i + HEADER_SIZE], receivedBytes[i]);
+		}
+	}
+
+	public void testSendAndReceive() {
+
+		byte[] inputBuffer = new byte[] {4, 0, 3, 4, 5, 7};
+		ByteArrayInputStream inStream = new ByteArrayInputStream(inputBuffer);
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+		MindstormConnectionImpl connection = new MindstormConnectionImpl(null);
+		Reflection.setPrivateField(connection, "nxtOutputStream", outStream);
+		Reflection.setPrivateField(connection, "nxtInputStream", inStream);
+
+		Command command = new Command(CommandType.DIRECT_COMMAND, CommandByte.SET_OUTPUT_STATE, false);
+		command.append((byte)0x1);
+		command.append((byte)0x2);
+		command.append((byte)0x3);
+
+		byte[] receivedBytes = connection.sendAndReceive(command);
+
+		byte[] sentBytes = outStream.toByteArray();
+
+		byte[] expectedMessage = command.getRawCommand();
+
+		assertEquals("Wrong message length. Before there should be a header with 2 bytes defining the message length",
+				expectedMessage.length + HEADER_SIZE, sentBytes.length);
+		assertEquals("Header should be the size of the message.", expectedMessage.length, sentBytes[0]);
+		assertEquals(0, sentBytes[1]);
+
+		for (int i = 0; i < expectedMessage.length; i++) {
+			assertEquals("Byte " + i + " is different", expectedMessage[i], sentBytes[i + HEADER_SIZE]);
+		}
+
+		assertEquals("Wrong message length. Before there should be a header with 2 bytes defining the message length",
+				inputBuffer.length - HEADER_SIZE, receivedBytes.length);
+
+		for (int i = 0; i < receivedBytes.length; i++) {
+			assertEquals("Byte " + i + " is different", inputBuffer[i + HEADER_SIZE], receivedBytes[i]);
+		}
+
+	}
 }
