09fbaaa407a86e55042293f6c00ba604498532b6
Andreas <maureder@student.tugraz.at>
2014-10-21 17:07:05 +0200

    implement first sensor tests

--- catroidTest/src/org/catrobat/catroid/test/mindstorms/nxt/SensorTests.java
+++ catroidTest/src/org/catrobat/catroid/test/mindstorms/nxt/SensorTests.java
@@ -23,5 +23,49 @@
 
 package org.catrobat.catroid.test.mindstorms.nxt;
 
-public class SensorTests {
+import android.test.AndroidTestCase;
+
+import org.catrobat.catroid.lego.mindstorm.MindstormCommand;
+import org.catrobat.catroid.lego.mindstorm.nxt.CommandByte;
+import org.catrobat.catroid.lego.mindstorm.nxt.CommandType;
+import org.catrobat.catroid.lego.mindstorm.nxt.sensors.NXTSensor;
+import org.catrobat.catroid.lego.mindstorm.nxt.sensors.NXTSensorMode;
+import org.catrobat.catroid.lego.mindstorm.nxt.sensors.NXTSensorType;
+import org.catrobat.catroid.lego.mindstorm.nxt.sensors.NXTTouchSensor;
+
+public class SensorTests extends AndroidTestCase {
+
+	private static final byte DIRECT_COMMAND_WITHOUT_REPLY = (byte) 0x80;
+	private static final byte PORT_NR_0 = 0;
+	private static final byte PORT_NR_1 = 1;
+	private static final byte PORT_NR_2 = 2;
+	private static final byte PORT_NR_3 = 3;
+
+	public void getSensorValue() {
+		MindstormTestConnection connection = new MindstormTestConnection();
+		NXTSensor touchSensor = new NXTTouchSensor(0, connection);
+
+		touchSensor.getValue();
+		MindstormCommand command = connection.getLastSentCommand();
+
+		byte[] rawCommand = command.getRawCommand();
+
+		assertEquals((byte)(DIRECT_COMMAND_WITHOUT_REPLY), rawCommand[0]);
+		assertEquals(CommandByte.GET_INPUT_VALUES.getByte(), rawCommand[1]);
+		assertEquals(PORT_NR_0, rawCommand[2]);
+	}
+
+	public void setSensorModeTouch(){
+//		MindstormTestConnection connection = new MindstormTestConnection();
+//		NXTSensor touchSensor = new NXTTouchSensor(0, connection);
+//
+//		touchSensor.getValue();
+//		MindstormCommand command = connection.getLastSentCommand();
+//
+//		byte[] rawCommand = command.getRawCommand();
+//
+//		assertEquals((byte)(DIRECT_COMMAND_WITHOUT_REPLY), rawCommand[0]);
+//		assertEquals(CommandByte.GET_INPUT_VALUES.getByte(), rawCommand[1]);
+//		assertEquals(PORT_NR_0, rawCommand[2]);
+	}
 }
